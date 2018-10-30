b88f31c227593acc7b0d212bae0f503a8b836641
Scott Trimber <scott.trimber@integnology.com>
2018-10-30 15:41:49 -0700

    GX-1291 adding java path to start service script

--- ci/bin/start-service.sh
+++ ci/bin/start-service.sh
@@ -1,3 +1,5 @@
+export JAVA_HOME=/var/vcap/packages/jdk/jdk/jdk1.8.0_181
+export PATH=$PATH:$JAVA_HOME/bin
 java -jar ${PKG_BASE_DIR}/${APPLICATION_NAME}/HelloWorld-0.0.2-SNAPSHOT.jar
 echo "Starting application"
 while [ $count -ge 0 ]; do
