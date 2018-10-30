2a6e64db26067bae8316bf496f3233c1d0da110d
Scott Trimber <scott.trimber@integnology.com>
2018-10-30 10:49:59 -0700

    GX-1291 adding start service and integration tests

--- ci/bin/start-service.sh
+++ ci/bin/start-service.sh
@@ -1,1 +1,17 @@
-java -jar HelloWorld-0.0.1-SNAPSHOT.jar 
+java -jar ${PKG_BASE_DIR}/${APPLICATION_NAME}/HelloWorld-0.0.2-SNAPSHOT.jar
+echo "Starting application"
+while [ $count -ge 0 ]; do
+    success=$(curl -s http://localhost:8082/HelloWorldExample/hello
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1;
+    let count-=1
+done
+if [ $success == 0 ]; then
+    echo "Deployment completed:$scansuccess"
+    exit 0
+else
+    echo "Deployment did not complete in 5 attempts, exiting"
+    exit 1
+fi
