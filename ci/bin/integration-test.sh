3f2d9bba9a08de3775714539018221723da63063
Scott Trimber <scott.trimber@integnology.com>
2018-10-31 11:15:51 -0700

    GX-1291 modify start service to list files

--- ci/bin/integration-test.sh
+++ ci/bin/integration-test.sh
@@ -1,3 +1,3 @@
 echo "Integration Test START"
-java -jar ${PKG_BASE_DIR}/${APPLICATION_NAME}/HelloWorld-0.0.1-SNAPSHOT.jar
+java -jar ${PKG_BASE_DIR}/HelloWorld/HelloWorld-0.0.1-SNAPSHOT.jar
 curl http://localhost:8082/HelloWorldExample/hello
