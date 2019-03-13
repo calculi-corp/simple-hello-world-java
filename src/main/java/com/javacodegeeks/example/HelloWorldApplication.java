e58374eadcc55765c53600baff5bbd1228e0cd6f
Trimber <scott.trimber@calculi.com>
2019-03-13 15:57:11 -0700

    adding bad code to trigger scanner issues

--- src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
@@ -9,4 +9,13 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+	public static void badCode(){
+		boolean test;
+		test = false;
+		if (test == true){/* don't test against boolean values */
+		System.out.println(-0.0f == 0.0f);
+		System.out.println(Float.compare (-0.0f, 0.0f) == 0 ? true :false);
+	}
+	}
+
 }
