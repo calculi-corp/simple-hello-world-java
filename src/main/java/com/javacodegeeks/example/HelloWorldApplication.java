70dc4b20e6daa3a4e2cfec0426bc8b9e433cea2e
Trimber <scott.trimber@calculi.com>
2019-03-13 16:09:15 -0700

    adding bad code to trigger scanner issues

--- src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
@@ -9,13 +9,53 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-	public static void badCode(){
+
+	public static void badCode() {
 		boolean test;
 		test = false;
-		if (test == true){/* don't test against boolean values */
-		System.out.println(-0.0f == 0.0f);
-		System.out.println(Float.compare (-0.0f, 0.0f) == 0 ? true :false);
+		// duplicate the follwing bad code so we some duplication metrics
+		// as well as code smells
+		if (test == true) {/* don't test against boolean values */
+			System.out.println(-0.0f == 0.0f);
+			System.out.println(Float.compare(-0.0f, 0.0f) == 0 ? true : false);
+		}
+		if (test == true) {/* don't test against boolean values */
+			System.out.println(-0.0f == 0.0f);
+			System.out.println(Float.compare(-0.0f, 0.0f) == 0 ? true : false);
+		}
+		if (test == true) {/* don't test against boolean values */
+			System.out.println(-0.0f == 0.0f);
+			System.out.println(Float.compare(-0.0f, 0.0f) == 0 ? true : false);
+		}
+		int day = 5;
+		String dayString;
+		// switch statement with int data type
+		switch (day) {
+		case 1:
+			dayString = "Monday";
+			break;
+		case 2:
+			dayString = "Tuesday";
+			break;
+		case 3:
+			dayString = "Wednesday";
+			break;
+		case 4:
+			dayString = "Thursday";
+			break;
+		case 5:
+			dayString = "Friday";
+			break;
+		case 6:
+			dayString = "Saturday";
+			break;
+		case 7:
+			dayString = "Sunday";
+			break;
+		default:
+			dayString = "Invalid day";
+			break;
+		}
+		System.out.println(dayString);
 	}
-	}
-
 }
