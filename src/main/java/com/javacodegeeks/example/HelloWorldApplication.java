c90fd7a694f8b913c2d9cd2dbdd22ae779e6f24a
Trimber <scott.trimber@calculi.com>
2019-03-13 16:33:27 -0700

    adding bad code to trigger scanner issues

--- src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldApplication.java
@@ -13,6 +13,9 @@ public class HelloWorldApplication {
 	public static boolean badCode() {
 		boolean test;
 		test = false;
+		String password;
+		password = "secret";
+
 		// duplicate the follwing bad code so we some duplication metrics
 		// as well as code smells
 		if (test == true) {/* don't test against boolean values */
@@ -56,7 +59,7 @@ public class HelloWorldApplication {
 		// no default, should trigger code issue
 		}
 		System.out.println(dayString);
-		
+
 		// multiple returns...bad idea!
 		if (test) {
 			return true;
@@ -64,5 +67,26 @@ public class HelloWorldApplication {
 			return false;
 		}
 	}
+	// this code should trigger a security violation
+	public boolean authenticate(javax.servlet.http.HttpServletRequest request, java.sql.Connection connection) throws SQLException {
+		String user = request.getParameter("user");
+		String pass = request.getParameter("pass");
+	  
+		String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'"; // Unsafe
+	  
+		// If the special value "foo' OR 1=1 --" is passed as either the user or pass, authentication is bypassed
+		// Indeed, if it is passed as a user, the query becomes:
+		// SELECT * FROM users WHERE user = 'foo' OR 1=1 --' AND pass = '...'
+		// As '--' is the comment till end of line syntax in SQL, this is equivalent to:
+		// SELECT * FROM users WHERE user = 'foo' OR 1=1
+		// which is equivalent to:
+		// SELECT * FROM users WHERE 1=1
+		// which is equivalent to:
+		// SELECT * FROM users
+	  
+		java.sql.Statement statement = connection.createStatement();
+		java.sql.ResultSet resultSet = statement.executeQuery(query); // Noncompliant
+		return resultSet.next();
+	  }
 
 }
